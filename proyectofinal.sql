57568a5e12ce6f57928ab83759341c0467ec7365
Nicolasfiguerasparras <Nicolasfiguerasparras@users.noreply.github.com>
2019-05-20 11:00:02 +0200

    Update proyectofinal.sql

--- proyectofinal.sql
+++ proyectofinal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-05-2019 a las 11:05:12
+-- Tiempo de generación: 20-05-2019 a las 10:59:15
 -- Versión del servidor: 10.1.36-MariaDB
 -- Versión de PHP: 7.2.11
 
@@ -60,7 +60,7 @@ CREATE TABLE `clients` (
 --
 
 INSERT INTO `clients` (`client_ID`, `name`, `surname`, `birth_date`, `phone`, `email`, `username`, `password`, `bill`) VALUES
-(2, 'Nicolás', 'Figueras Parras', '1999-03-22', 639941992, 'nicolasfiguerasparras@gmail.com', 'nicoClient', 'nicoClient', 0),
+(2, 'Nicolás', 'Figueras Parras', '1999-03-22', 639941992, 'nicolasfiguerasparras@gmail.com', 'nicoClient', 'nicoClient', 350),
 (3, 'Fernando', 'Juanez ', '1998-12-03', 626451200, 'fernando@gmail.com', 'fernando', 'fernando', 0);
 
 -- --------------------------------------------------------
@@ -102,6 +102,13 @@ CREATE TABLE `payment` (
   `date` date NOT NULL,
   `type` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `payment`
+--
+
+INSERT INTO `payment` (`payment_ID`, `quantity`, `client_ID`, `worker_ID`, `date`, `type`) VALUES
+(3, 350.00, 2, 1, '2019-05-19', 0);
 
 -- --------------------------------------------------------
 
@@ -215,7 +222,7 @@ ALTER TABLE `lawers`
 -- AUTO_INCREMENT de la tabla `payment`
 --
 ALTER TABLE `payment`
-  MODIFY `payment_ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `payment_ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `tasks`
